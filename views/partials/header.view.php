533d1429e7613ab8c2851d2b33f71db5ef555014
Hilal Ahmad <hilaldev123@gmail.com>
2023-07-14 16:17:32 +0500

    Ready With 1 Issue

--- views/partials/header.view.php
+++ views/partials/header.view.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Taxi Booking Site</title>
+    <title>VIP minibus hire</title>
     <script src="https://js.stripe.com/v3/"></script>
     <script src="https://cdn.tailwindcss.com"></script>
 
